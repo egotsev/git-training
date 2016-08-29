70452764848f8ad11b9f324c738e28e794ec18b4
Emil Gotsev <emil.gotsev@scalefocus.com>
2016-08-29 00:10:31 -0700

    asdasd

--- feature-7.f
+++ feature-7.f
@@ -1,9 +1,10 @@
 def Blah
 	def get_message()
-		message
+		author + ": " + message
 
-	def set_message(m)
+	def set_message(m, a)
 		message = m
+		author = a
 
 	def reset()
 		message = "Hello from the other side"
